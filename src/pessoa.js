444bcf796ca576a7fd82c587f541ccca62b1c79c
enokjanuario <ejrocha07@gmail.com>
2025-03-17 21:34:53 -0300

    Alterando a ordem de checagem do atributo idade de modo a evitar comparações desnecessárias (refactor)

--- src/pessoa.js
+++ src/pessoa.js
@@ -1,15 +1,14 @@
 class Pessoa {
   ehMaiorDeIdade(idade) {
-    if (idade < 0) {
-        throw new Error('A idade não pode ser negativa');
-    }
-
     if (typeof idade !== 'number') {
       throw new Error('A idade deve ser um número');
     }
 
-    return idade >= 18;
+    if (idade < 0) {
+      throw new Error('A idade não pode ser negativa');
+    }
 
+    return idade >= 18;
   }
 }
 
